c70b7688d4c4659571e3dceb0f0d23a0572891c3
Lawrence Kelly <lkelly@givelify.com>
2021-04-21 10:34:25 -0700

    add automated uploader to s3 east1

---
+++ auto_rds_log_downloader.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+
+
+# get the slow query file logs list
+
+aws rds describe-db-log-files --region us-east-1 --db-instance-identifier givelify-aurora-vpc | grep slow > list.txt
+
+# remove the logiles as we append the results
+
+rm output.txt
+rm logfile.txt
+rm aws_logfile.txt
+rm aws_outfile.txt
+
+# loop through the logfile list
+
+for i in `cat list.txt`; do
+
+    file=`echo $i | sed 's/^.*\///'| sed 's/",//'`
+    file2=`echo $file | sed 's/"LogFileName":/''/'` 
+    echo $file2 >> output.txt
+
+    f=`echo "$i" | sed 's/^.*"slow/slow/' | sed 's/",//'` 
+    g=`echo $f | sed 's/"LogFileName":/''/'` 
+    echo $g >> logfile.txt 
+done;
+
+# remove blank lines
+    
+grep "\S" logfile.txt >> aws_logfile.txt
+grep "\S" output.txt >> aws_outfile.txt
+
+# join files and loop over once
+
+paste -- "aws_logfile.txt" "aws_outfile.txt" |
+while IFS=$'\t' read -r file1 file2 rest; do
+	# echo  $file1
+	# echo $file2
+
+# run the loader python script to upload the files
+
+python3 rds_log_downloader.py --s3_bucket database-admin-repo --region us-east-1 --db givelify-aurora-vpc --logfile $file1 --output $file2
+
+done
